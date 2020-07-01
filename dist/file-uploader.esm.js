157a79b2b0d36656f0f406226aaa84dd0142a687
Ahmed Fathy <aliraqi.dev@gmail.com>
2020-07-01 05:18:31 +0200

    reset input after upload

--- dist/file-uploader.esm.js
+++ dist/file-uploader.esm.js
@@ -131,6 +131,7 @@ var script = {
               this$1.files.push(file[0]);
               this$1.values.push(response.token);
               this$1.complete();
+              input.value = '';
             })
             .catch(function (error) {
               this$1.pending--;
@@ -355,11 +356,11 @@ var __vue_staticRenderFns__ = [];
   /* style */
   var __vue_inject_styles__ = function (inject) {
     if (!inject) { return }
-    inject("data-v-cdc052c2_0", { source: ".mw-100[data-v-cdc052c2]{max-width:100%!important}.mb-4[data-v-cdc052c2]{margin-bottom:1.5rem!important}.px-3[data-v-cdc052c2]{padding-right:1rem!important}.px-3[data-v-cdc052c2]{padding-left:1rem!important}.text-muted[data-v-cdc052c2]{color:#6c757d!important}.uploader .img-item[data-v-cdc052c2]:not(.add){position:relative;text-align:center;border:2px dashed gray;border-radius:15px 0;overflow:hidden}.uploader .img-item:not(.add) img[data-v-cdc052c2]{text-align:center;max-width:100%}@media (min-width:992px){.uploader .img-item[data-v-cdc052c2]{width:140px}.uploader .img-item:not(.add) img[data-v-cdc052c2]{height:96px}}@media (min-width:768px) and (max-width:991.98px){.uploader .img-item[data-v-cdc052c2]{width:170px}.uploader .img-item:not(.add) img[data-v-cdc052c2]{height:126px}}@media (max-width:767.98px){.uploader .img-item:not(.add) img[data-v-cdc052c2]{height:96px}}.uploader .img-item:not(.add) a.delete[data-v-cdc052c2]{position:absolute;top:0;right:0;color:#fff!important;background:red;padding:1px;text-align:center;width:20px;height:20px;font-size:13px}.uploader .img-item:not(.add) .size[data-v-cdc052c2]{position:absolute;width:100%;left:0;bottom:0;background:#808080d6;border-radius:0 0 15px 0;color:#fff;font-weight:700}.uploader .add[data-v-cdc052c2]{cursor:pointer;border:2px dashed gray;border-radius:15px 0;display:flex;align-items:center}@media (min-width:992px){.uploader .add[data-v-cdc052c2]{height:100px;width:140px}}@media (min-width:768px) and (max-width:991.98px){.uploader .add[data-v-cdc052c2]{height:130px}}@media (max-width:767.98px){.uploader .add[data-v-cdc052c2]{height:100px;width:140px}}.uploader .add img[data-v-cdc052c2],.uploader .add svg[data-v-cdc052c2]{width:30%;margin:0 auto}", map: undefined, media: undefined });
+    inject("data-v-36659f24_0", { source: ".mw-100[data-v-36659f24]{max-width:100%!important}.mb-4[data-v-36659f24]{margin-bottom:1.5rem!important}.px-3[data-v-36659f24]{padding-right:1rem!important}.px-3[data-v-36659f24]{padding-left:1rem!important}.text-muted[data-v-36659f24]{color:#6c757d!important}.uploader .img-item[data-v-36659f24]:not(.add){position:relative;text-align:center;border:2px dashed gray;border-radius:15px 0;overflow:hidden}.uploader .img-item:not(.add) img[data-v-36659f24]{text-align:center;max-width:100%}@media (min-width:992px){.uploader .img-item[data-v-36659f24]{width:140px}.uploader .img-item:not(.add) img[data-v-36659f24]{height:96px}}@media (min-width:768px) and (max-width:991.98px){.uploader .img-item[data-v-36659f24]{width:170px}.uploader .img-item:not(.add) img[data-v-36659f24]{height:126px}}@media (max-width:767.98px){.uploader .img-item:not(.add) img[data-v-36659f24]{height:96px}}.uploader .img-item:not(.add) a.delete[data-v-36659f24]{position:absolute;top:0;right:0;color:#fff!important;background:red;padding:1px;text-align:center;width:20px;height:20px;font-size:13px}.uploader .img-item:not(.add) .size[data-v-36659f24]{position:absolute;width:100%;left:0;bottom:0;background:#808080d6;border-radius:0 0 15px 0;color:#fff;font-weight:700}.uploader .add[data-v-36659f24]{cursor:pointer;border:2px dashed gray;border-radius:15px 0;display:flex;align-items:center}@media (min-width:992px){.uploader .add[data-v-36659f24]{height:100px;width:140px}}@media (min-width:768px) and (max-width:991.98px){.uploader .add[data-v-36659f24]{height:130px}}@media (max-width:767.98px){.uploader .add[data-v-36659f24]{height:100px;width:140px}}.uploader .add img[data-v-36659f24],.uploader .add svg[data-v-36659f24]{width:30%;margin:0 auto}", map: undefined, media: undefined });
 
   };
   /* scoped */
-  var __vue_scope_id__ = "data-v-cdc052c2";
+  var __vue_scope_id__ = "data-v-36659f24";
   /* module identifier */
   var __vue_module_identifier__ = undefined;
   /* functional template */
